3b9e73c4bbd37f0b983490d0e93ec9381b2403d4
suny575 <merocala869@gmail.com>
2025-11-06 01:03:27 +0300

    updted icons

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
-import '../styles/Footer.css';
-import { FaGithub, FaInstagram, FaTelegram, FaEnvelope } from 'react-icons/fa';
+import React from "react";
+import "../styles/Footer.css";
+import { FaGithub, FaInstagram, FaTelegram, FaEnvelope } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="social-icon">
-        <a className="social-icon" href="https://github.com/suny575" target="_blank" rel="noreferrer"><FaGithub /></a>
-        <a className="social-icon" href="#https/instagram/dagm203" target="_blank" rel="noreferrer"><FaInstagram /></a>
-        <a className="social-icon" href="https://t.me/dagmit21" target="_blank" rel="noreferrer"><FaTelegram /></a>
-        <a className="social-icon" href="mailto:merocala869@gmail.com"><FaEnvelope /></a>
+        <a
+          className="social-icon"
+          href="https://github.com/suny575"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          className="social-icon"
+          href="https://www.instagram.com/dagm203"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaInstagram />
+        </a>
+
+        <a
+          className="social-icon"
+          href="https://t.me/dagmit21"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaTelegram />
+        </a>
+        <a className="social-icon" href="mailto:merocala869@gmail.com">
+          <FaEnvelope />
+        </a>
       </div>
       <p>© 2025 Tsehay• Crafted with 💜 and React</p>
     </footer>
