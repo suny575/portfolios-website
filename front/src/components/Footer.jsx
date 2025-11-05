14082f114210c59c2de53f34dc37c4a43e8cbe22
suny575 <merocala869@gmail.com>
2025-11-06 00:44:54 +0300

    icon added

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/Footer.css';
-import { FaGithub, FaLinkedin, FaTelegram, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaInstagram, FaTelegram, FaEnvelope } from 'react-icons/fa';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="social-icon">
         <a className="social-icon" href="https://github.com/suny575" target="_blank" rel="noreferrer"><FaGithub /></a>
-        <a className="social-icon" href="#" target="_blank" rel="noreferrer"><FaLinkedin /></a>
+        <a className="social-icon" href="#https/instagram/dagm203" target="_blank" rel="noreferrer"><FaInstagram /></a>
         <a className="social-icon" href="https://t.me/dagmit21" target="_blank" rel="noreferrer"><FaTelegram /></a>
         <a className="social-icon" href="mailto:merocala869@gmail.com"><FaEnvelope /></a>
       </div>
